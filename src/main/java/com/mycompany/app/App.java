aa3dde172f3211fc472b910eb5169060bc034c57
kursadsaka <ksaka@etu.edu.tr>
2019-02-02 18:34:00 +0300

    Changing the function, tests and compute.mustache

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -11,64 +11,85 @@ import java.util.Map;
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
 
-public class App
-{
-    public static boolean search(ArrayList<Integer> array, int e) {
-      System.out.println("inside search");
-      if (array == null) return false;
+public class App {
+	public static boolean searchAndSum(ArrayList<Integer> array, int e, ArrayList<Integer> array2, int e2) {
+		System.out.println("inside searchAndSum");
+		boolean islem1 = false;
+		boolean islem2 = false;
+		int sum = 0;
 
-      for (int elt : array) {
-        if (elt == e) return true;
-      }
-      return false;
-    }
+		if (array != null)
+			for (int elt : array)
+				if (elt == e * e)
+					islem1 = true;
 
-    public static void main(String[] args) {
-        port(getHerokuAssignedPort());
+		if (array2 != null)
+			for (int elt : array2)
+				sum += elt * e;
 
-        get("/", (req, res) -> "Hello, World");
+		if (sum == e2)
+			islem2 = true;
 
-        post("/compute", (req, res) -> {
-          //System.out.println(req.queryParams("input1"));
-          //System.out.println(req.queryParams("input2"));
+		return islem1 && islem2;
 
-          String input1 = req.queryParams("input1");
-          java.util.Scanner sc1 = new java.util.Scanner(input1);
-          sc1.useDelimiter("[;\r\n]+");
-          java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
-          while (sc1.hasNext())
-          {
-            int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
-            inputList.add(value);
-          }
-          System.out.println(inputList);
+	}
 
+	public static void main(String[] args) {
+		port(getHerokuAssignedPort());
 
-          String input2 = req.queryParams("input2").replaceAll("\\s","");
-          int input2AsInt = Integer.parseInt(input2);
+		get("/", (req, res) -> "Hello, World");
 
-          boolean result = App.search(inputList, input2AsInt);
+		post("/compute", (req, res) -> {
+			// System.out.println(req.queryParams("input1"));
+			// System.out.println(req.queryParams("input2"));
 
-         Map map = new HashMap();
-          map.put("result", result);
-          return new ModelAndView(map, "compute.mustache");
-        }, new MustacheTemplateEngine());
+			String input1 = req.queryParams("input1");
+			java.util.Scanner sc1 = new java.util.Scanner(input1);
+			sc1.useDelimiter("[;\r\n]+");
+			java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+			while (sc1.hasNext()) {
+				int value = Integer.parseInt(sc1.next().replaceAll("\\s", ""));
+				inputList.add(value);
+			}
+			System.out.println(inputList);
 
+			String input2 = req.queryParams("input2").replaceAll("\\s", "");
+			int input2AsInt = Integer.parseInt(input2);
+			System.out.println(input2AsInt);
 
-        get("/compute",
-            (rq, rs) -> {
-              Map map = new HashMap();
-              map.put("result", "not computed yet!");
-              return new ModelAndView(map, "compute.mustache");
-            },
-            new MustacheTemplateEngine());
-    }
+			String input3 = req.queryParams("input3");
+			java.util.Scanner sc2 = new java.util.Scanner(input3);
+			sc2.useDelimiter("[;\r\n]+");
+			java.util.ArrayList<Integer> inputList2 = new java.util.ArrayList<>();
+			while (sc2.hasNext()) {
+				int value = Integer.parseInt(sc2.next().replaceAll("\\s", ""));
+				inputList2.add(value);
+			}
+			System.out.println(inputList2);
 
-    static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
-    }
+			String input4 = req.queryParams("input4").replaceAll("\\s", "");
+			int input4AsInt = Integer.parseInt(input4);
+			System.out.println(input4AsInt);
+
+			boolean result = App.searchAndSum(inputList, input2AsInt, inputList2, input4AsInt);
+
+			Map map = new HashMap();
+			map.put("result", result);
+			return new ModelAndView(map, "compute.mustache");
+		}, new MustacheTemplateEngine());
+
+		get("/compute", (rq, rs) -> {
+			Map map = new HashMap();
+			map.put("result", "not computed yet!");
+			return new ModelAndView(map, "compute.mustache");
+		}, new MustacheTemplateEngine());
+	}
+
+	static int getHerokuAssignedPort() {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null) {
+			return Integer.parseInt(processBuilder.environment().get("PORT"));
+		}
+		return 4567; // return default port if heroku-port isn't set (i.e. on localhost)
+	}
 }
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -9,52 +9,68 @@ import java.util.Arrays;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Rigorous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
 
-public void testFound() {
-      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      assertTrue(new App().search(array, 4));
-    }
+	public void testFoundTheSquareAndSum() {
+		ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 25));
+		ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(3, 4, 5));
+		assertTrue(new App().searchAndSum(array, 5, array2, 60));
+	}
 
-    public void testNotFound() {
-      ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-      assertFalse(new App().search(array, 5));
-    }
+	public void testNotFoundTheSquare() {
+		ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
+		ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(3, 4, 5));
+		assertFalse(new App().searchAndSum(array, 5, array2, 60));
+	}
 
-    public void testEmptyArray() {
-      ArrayList<Integer> array = new ArrayList<>();
-      assertFalse(new App().search(array, 1));
-    }
+	public void testCouldNotGetTheSum() {
+		ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 25));
+		ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(3, 4, 5));
+		assertFalse(new App().searchAndSum(array, 5, array2, 61));
+	}
 
-    public void testNull() {
-      assertFalse(new App().search(null, 1));
-    }
+	public void testEmptyArray1() {
+		ArrayList<Integer> array = new ArrayList<>();
+		ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(3, 4, 5));
+		assertFalse(new App().searchAndSum(array, 5, array2, 60));
+	}
+
+	public void testEmptyArray2() {
+		ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 25));
+		ArrayList<Integer> array2 = new ArrayList<>();
+		assertFalse(new App().searchAndSum(array, 5, array2, 60));
+	}
+
+	public void testNullArray1() {
+		ArrayList<Integer> array2 = new ArrayList<>(Arrays.asList(3, 4, 5));
+		assertFalse(new App().searchAndSum(null, 5, array2, 60));
+	}
+
+	public void testNullArray2() {
+		ArrayList<Integer> array = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 25));
+		assertFalse(new App().searchAndSum(array, 5, null, 60));
+	}
 
 }
